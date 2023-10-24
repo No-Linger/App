35960e4d598ac63091af5b08847c7eb4857f12c8
MiguelGurtubay <miguelgarcia@ezigna.com>
2023-10-23 18:36:19 -0600

    Solo uno

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -7,6 +7,7 @@ import Svg,{Ellipse, Image,ClipPath} from 'react-native-svg';
 import Imagen from '../components/imagen.js';
 import { Dimensions } from "react-native";
 import Caca from "../components/caca";
+
 import Animated,{
   interpolate, 
   useSharedValue, 
@@ -23,14 +24,37 @@ const logoImageSource = require('../../assets/logoOxxo.png');
 export default function Login(props) {
     const {height, width}= Dimensions.get('window');
     const imagePosition = useSharedValue(1)
-
+    const formButtonScale = useSharedValue(1);
     const imageAnimatedStyle=useAnimatedStyle(()=>{
       const interpolation=interpolate(imagePosition.value,[0,1],[-height/2,0])
       return {
         transform: [{translateY:withTiming(interpolation,{duration:1000})}]
       }
     })
-
+    const buttonsAnimatedStyle = useAnimatedStyle(()=>{
+      const interpolation =interpolate(imagePosition.value, [0,1],[250,0])
+      return{
+        opacity:withTiming(imagePosition.value,{duration:500}),
+        transform:[{translateY:withTiming(interpolation,{duration:1000})}]
+      }
+    })
+    const closeButtonContainerStyle=useAnimatedStyle(()=>{
+      const interpolation=interpolate(imagePosition.value, [0,1],[180,360])
+      return {
+        opacity:withTiming(imagePosition.value===1?0:1,{duration:800}),
+        transform:[{rotate:withTiming(interpolation+"deg",{duration:1000})}]
+      }
+    })
+    const formAnimatedStyle=useAnimatedStyle(()=>{
+      return{
+        opacity:imagePosition.value===0?withDelay(400,withTiming(1,{duration:800})):withTiming(0,{duration:300})
+      }
+    })
+    const formButtonAnimatedStyle=useAnimatedStyle(()=>{
+      return{
+        transform:[{scale:formButtonScale.value}]
+      }
+    })
     const loginHandler=()=>{
       imagePosition.value=0
     }
@@ -44,18 +68,26 @@ export default function Login(props) {
     const [password, setPassword] = React.useState('');
     const AUTH = authClient
 
-    const signIn = async() =>{
-        try{
-            const response = await signInWithEmailAndPassword(AUTH,username,password)
-            console.log(response)
-            props.onClick()
-        }
-        catch(error){
-            console.log(error)
-        }
-    }
+    // const signIn = async() => {
+    //   try {
+    //     const response = await signInWithEmailAndPassword(AUTH, username, password);
+    //     console.log(response);
+    
+    //     // Verificar que el inicio de sesión fue exitoso antes de navegar
+    //     if (response) {
+    //       // Obtener el objeto de navegación
+    //       //const navigation = useNavigation();
+    
+    //       // Navegar a la pantalla de estadísticas
+    //       //navigation.navigate('Estadística'); // Debes usar el nombre de la pantalla en la navegación
+    //     }
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // };
 
-    const handleClick = () => {
+    
+    
         // Add your authentication logic here.
         // if (username === 'testuser' && password === 'testpassword') {
         //   Alert.alert('Login Successful', 'You are now logged in!');
@@ -63,16 +95,29 @@ export default function Login(props) {
         //   Alert.alert('Login Failed', 'Invalid username or password');
         // }
 
-        signIn()
+        const signIn = async() =>{
+          try{
+              const response = await signInWithEmailAndPassword(AUTH,username,password)
+              console.log(response)
+              props.onClick()
+          }
+          catch(error){
+              console.log(error)
+          }
       }
+  
+        const startButtonAnimation = () => {
+          formButtonScale.value = withSequence(withSpring(1.5), withSpring(1));
+        };
+        
 
       return (
         <View style={styles.container}> 
-          <Animated.View style={StyleSheet.absoluteFill}>
-            <Svg height={height} width={width}>
-              {/* <ClipPath id="clipPathId">
+          <Animated.View style={[StyleSheet.absoluteFill,imageAnimatedStyle]}>
+            <Svg height={height+100} width={width}>
+              <ClipPath id="clipPathId">
                 <Ellipse cx={width/2} rx={height} ry={height+100}/>
-              </ClipPath> */}
+              </ClipPath>
               <Image href={require("../../assets/cruz.jpg")} 
               width={width+100} 
               height={height+100}
@@ -80,36 +125,51 @@ export default function Login(props) {
               clipPath="url(#clipPathId)"
               />
             </Svg>
-            <View style={styles.closeButtomContainer}>
-              <Text>X</Text>
-            </View>
+            <Animated.View style={[styles.closeButtonContainer,closeButtonContainerStyle]}>
+              <Text onPress={()=>imagePosition.value=1}>X</Text>
+            </Animated.View>
           </Animated.View>
           <View style={styles.bottomContainer}>
-            <Pressable style={styles.button} onPress={loginHandler}>
-              <Text style={styles.buttonText}>LOG IN</Text>
-            </Pressable>
-            <Pressable style={styles.button} onPress={registerHandler}>
-              <Text style={styles.buttonText}>REGISTER</Text>
-            </Pressable>
+            <Animated.View style={buttonsAnimatedStyle}>
+              <Pressable style={styles.button} onPress={loginHandler}>
+                <Text style={styles.buttonText}>LOG IN</Text>
+              </Pressable>
+            </Animated.View>
           </View>
-          {/* <View style={styles.formInputContainer}>
+          <Animated.View style={[styles.formInputContainer,formAnimatedStyle]}>
             <TextInput
               placeholder="Email"
               placeholderTextColor="black"
               style={styles.textInput}
+              onChangeText={(text) => setUsername(text)}
+               value={username}
             />
             <TextInput
               placeholder="PassWord"
               placeholderTextColor="black"
               style={styles.textInput}
+              onChangeText={(text) => setPassword(text)}
+              value={password}
+              secureTextEntry
             />
-            <View style={styles.formButtom}>
-              <Text style={styles.buttomText}>LOG IN</Text>
-            </View>
-          </View> */}
-          </View>
-        
-
+            <Animated.View style={[styles.formButtom, formButtonAnimatedStyle]}>
+              <Pressable   onPress={async () => {
+    startButtonAnimation();
+    signIn()// Iniciar la animación al presionar el botón
+    // try {
+    //   const response = await signInWithEmailAndPassword(AUTH, username, password);
+    //   console.log(response);
+    //   props.onClick();
+    // } catch (error) {
+    //   console.log(error);
+    //     }
+       }}
+    >
+              <Text style={styles.buttonText}>LOG IN</Text>
+              </Pressable>
+            </Animated.View>
+          </Animated.View>
+        </View>
       );
     }
     
