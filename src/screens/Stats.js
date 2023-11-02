0e715dac9fdb5bb368b0896ca153d9bbbd952a87
A01746459 <A01746459@itesm.mx>
2023-11-01 18:54:22 -0600

    cambios ceviche

--- src/screens/Stats.js
+++ src/screens/Stats.js
@@ -1,335 +1,122 @@
-import React, { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  ScrollView,
-  Button,
-} from "react-native";
+// Importamos las librerías de React y componentes necesarios
+import React, { useEffect, useState } from "react";
+import { View, Button, Text } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import NetInfo from "@react-native-community/netinfo"; // Import NetInfo
-import Icon from "react-native-vector-icons/FontAwesome";
-import { LottieAnimation } from "../components";
-import { saveDataToAsyncStorage } from "../services/fetchService";
-import { syncDataToMongoDB } from "../services/statsUpdate";
+import NetInfo from "@react-native-community/netinfo";
 
-export default function Stats({ navigation }) {
-  const [statData, setStatData] = useState(null); // Data from JSON
-  const [currentDate, setCurrentDate] = useState(new Date().toLocaleDateString());
+export default function TestStats() {
+  // Función para obtener un número entero aleatorio en un rango específico
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-  const [records, setRecords] = useState({}); // Object with all the data saved.
-  const [isConnected, setIsConnected] = useState(true); // To track internet connection.
+  // Función para obtener un elemento aleatorio de un arreglo
+  function getRandomArrayElement(arr) {
+    return arr[getRandomInt(0, arr.length - 1)];
+  }
 
-  const [showRecords, setShowRecords] = useState(false);
-  const [selectedDate, setSelectedDate] = useState(null);
+  // Función para generar un JSON falso para simular los datos capturados
+  const getFakeJson = () => {
+    const precision = getRandomInt(97, 100);
+    const fecha = new Date().toLocaleDateString();
+    const hora = new Date().toLocaleTimeString();
+    const planograma = getRandomArrayElement(["Sabritas", "CocaCola", "Barcel"]);
+    const sucrusal = 123456;
+    const fakeJSON = { planograma, fecha, hora, precision, sucrusal};
+    return fakeJSON;
+  };
 
-  useEffect(() => {
-    const intervalId = setInterval(
-      () => setCurrentDate(new Date().toLocaleDateString()),
-      1000
-    );
-    return () => clearInterval(intervalId);
-  }, []);
-
-  useEffect(() => {
-    // Check for internet connection on component mount
-    NetInfo.addEventListener((state) => {
-      setIsConnected(state.isConnected);
-    });
-
-    const fetchData = async () => {
-      try {
-        // Retrieve data from AsyncStorage
-        const data = await AsyncStorage.getItem("statData");
-
-        // Parse the data into parsedData, or initialize it as an empty array
-        const parsedData = data ? JSON.parse(data) : [];
-
-        // Set the parsedData in the state
-        setRecords(parsedData);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    // When the internet connection becomes available, sync the data with MongoDB
-    if (isConnected) {
-      syncDataWithMongoDB();
-    }
-  }, [isConnected]);
-
-  const syncDataWithMongoDB = async () => {
+  // Función para subir los datos a la API
+  const uploadData = async (data) => {
     try {
-      for (const date in records) {
-        for (const data of records[date]) {
-          await syncDataToMongoDB(data);
+      let response = await fetch("http://10.48.70.252:8082/postStats", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const d = await response.json();
+      console.log(d);
+      if (response.ok) {
+        return true;
+      }
+      return false;
+    } catch (err) {
+      return false;
+    }
+  };
+
+  // Función para intentar subir los registros almacenados
+  const tryUploadRecords = async (data) => {
+    console.log("try upload", data);
+    if (Array.isArray(data)) {
+      for (let i = 0; i < data.length; i++) {
+        if (!data[i]["uploaded"]) {
+          let state = await uploadData(data[i]);
+          data[i]["uploaded"] = state;
         }
       }
-    } catch (error) {
-      console.error("Error uploading data to MongoDB:", error);
+      return data;
+    } else {
+      return [];
     }
   };
 
-  const saveDataAndLoadFecha = async () => {
-    try {
-      // Capture data immediately
-      const jsonData = await saveDataToAsyncStorage();
-      setStatData(jsonData);
+  // Manejador del botón para capturar datos
+  const handleCapturar = async () => {
+    const fakeData = getFakeJson();
+    let data = await AsyncStorage.getItem("capturas");
+    data = JSON.parse(data);
+    if (!Array.isArray(data)) {
+      data = [];
+    }
+    fakeData["uploaded"] = false;
+    data.push(fakeData);
+    let newRecords = await tryUploadRecords(data);
+    await AsyncStorage.setItem("capturas", JSON.stringify(newRecords));
+  };
 
-      // Create a temporary array to hold data that needs to be uploaded
-      const tempRecords = { ...records };
-      if (!tempRecords[currentDate]) {
-        tempRecords[currentDate] = [];
+  // Estado para manejar la conexión a internet
+  const [isConnected, setIsConnected] = useState(null);
+
+  // useEffect para manejar cambios en la conexión a internet
+  useEffect(() => {
+    // Suscribimos al evento de cambio de conexión
+    let subscription = NetInfo.addEventListener(async (state) => {
+      console.log(state.isConnected);
+      setIsConnected(state.isConnected);
+      if (state.isConnected) {
+        let data = await AsyncStorage.getItem("capturas");
+        data = JSON.parse(data);
+        await tryUploadRecords(data);
       }
-      tempRecords[currentDate].push(jsonData);
+    });
 
-      // Log the tempRecords to see the captures
-      console.log("Temp Records:", tempRecords);
+    // Limpiamos la suscripción al desmontar el componente
+    return () => {
+      subscription();
+    };
+  }, [isConnected]);
 
-      // Display the data immediately
-      setRecords(tempRecords);
-
-      // If there's an internet connection, sync data with MongoDB immediately
-      if (isConnected) {
-        await syncDataToMongoDB(jsonData);
-      }
-
-      // Save updated tempRecords to AsyncStorage
-      await AsyncStorage.setItem("statData", JSON.stringify(tempRecords));
-    } catch (error) {
-      console.error("Error:", error);
-    }
+  // Función para obtener los datos almacenados
+  const getData = async () => {
+    let data = await AsyncStorage.getItem("capturas");
+    console.log(data);
   };
 
-  const handleClearData = async () => {
-    try {
-      await AsyncStorage.removeItem("statData");
-      setRecords({});
-      console.log("Data cleared successfully.");
-    } catch (error) {
-      console.error("Error clearing data:", error);
-    }
+  // Función para borrar los datos almacenados
+  const clearData = async () => {
+    await AsyncStorage.setItem("capturas", JSON.stringify([]));
   };
 
+  // Renderizado del componente
   return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.title}>¡Hola! Hoy es: {currentDate}</Text>
-      {records[currentDate] && records[currentDate].length > 0 ? (
-        <View style={styles.statsContainer}>
-          <Text style={styles.statsTitle}>Captura</Text>
-          {statData && (
-            <>
-              <View style={styles.statsRow}>
-                <Text style={styles.leftText}>Fecha:</Text>
-                <Text>{statData.fecha}</Text>
-              </View>
-              <View style={styles.statsRow}>
-                <Text style={styles.leftText}>Hora:</Text>
-                <Text>{statData.hora}</Text>
-              </View>
-              <View style={styles.statsRow}>
-                <Text style={styles.leftText}>Porcentaje modelo:</Text>
-                <Text style={styles.greenText}>{statData.porcentaje_modelo}%</Text>
-              </View>
-              <View style={styles.statsRow}>
-                <Text style={styles.leftText}>Porcentaje error:</Text>
-                <Text style={styles.redText}>{statData.porcentaje_error}%</Text>
-              </View>
-            </>
-          )}
-        </View>
-      ) : (
-        <View style={styles.noCaptureContainer}>
-          <Text style={styles.noCaptureText}>
-            Aún no has realizado ninguna captura hoy.
-          </Text>
-          <LottieAnimation
-            source={require("../../assets/lotties/potatoeWalking.json")}
-            width={"50"}
-            height={"50"}
-          />
-          <TouchableOpacity
-            style={styles.captureButton}
-            onPress={saveDataAndLoadFecha}
-          >
-            <Text style={styles.captureButtonText}>Capturar</Text>
-          </TouchableOpacity>
-        </View>
-      )}
-      <View style={styles.clearDataContainer}>
-        <Button title="Clear All Data" onPress={handleClearData} />
-      </View>
-      <TouchableOpacity
-        style={styles.captureButton}
-        onPress={saveDataAndLoadFecha}
-      >
-        <Text style={styles.captureButtonText}>Capturar</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => setShowRecords(!showRecords)}
-        style={styles.showRecordsButton}
-      >
-        <View style={styles.buttonContainer}>
-          <Text>Datos Guardados</Text>
-          <Icon name={showRecords ? "arrow-up" : "arrow-down"} size={16} />
-        </View>
-      </TouchableOpacity>
-      {showRecords && (
-        <View style={styles.recordsContainer}>
-          {Object.entries(records).map(([fecha, captureArray]) => (
-            <View key={fecha}>
-              <TouchableOpacity
-                style={styles.buttonContainer}
-                onPress={() =>
-                  setSelectedDate(selectedDate === fecha ? null : fecha)
-                }
-              >
-                <Text style={styles.boldText}>Fecha: {fecha}</Text>
-                <Icon
-                  name={selectedDate === fecha ? "folder-open" : "folder"}
-                  size={15}
-                />
-              </TouchableOpacity>
-              {selectedDate === fecha &&
-                captureArray.map((capture, captureIndex) => (
-                  <View key={captureIndex} style={styles.captureInfo}>
-                    <View style={styles.captureRow}>
-                      <Text style={styles.leftText}>Hora:</Text>
-                      <Text>{capture.hora}</Text>
-                    </View>
-                    <View style={styles.captureRow}>
-                      <Text style={styles.leftText}>Porcentaje modelo:</Text>
-                      <Text style={styles.greenText}>
-                        {capture.porcentaje_modelo}%
-                      </Text>
-                    </View>
-                    <View style={styles.captureRow}>
-                      <Text style={styles.leftText}>Porcentaje error:</Text>
-                      <Text style={styles.redText}>
-                        {capture.porcentaje_error}%
-                      </Text>
-                    </View>
-                  </View>
-                ))}
-            </View>
-          ))}
-        </View>
-      )}
-    </ScrollView>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Connected : {isConnected ? "Sí" : "No"}</Text>
+      <Button onPress={handleCapturar} title="Capturar" />
+      <Button onPress={getData} title="Datos" />
+      <Button onPress={clearData} title="Borrar" />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-    margin: 5,
-  },
-  title: {
-    fontSize: 15,
-    fontWeight: "bold",
-    padding: 3,
-    marginBottom: 10,
-  },
-  statsContainer: {
-    marginTop: 10,
-    backgroundColor: "gainsboro",
-    overflow: "hidden",
-    borderRadius: 10,
-    padding: 10,
-    
-  },
-  statsTitle: {
-    textAlign: "center",
-    marginBottom: 5,
-    fontWeight: "bold",
-    overflow:'hidden',
-    borderRadius: 5,
-    padding: 5,
-  },
-  greenText: {
-    color: "green",
-    marginBottom: 3,
-  },
-  redText: {
-    marginBottom: 5,
-    color: "red",
-  },
-  noCaptureContainer: {
-    backgroundColor: "gainsboro",
-    overflow: "hidden",
-    borderRadius: 20,
-    alignItems: "center",
-    shadowOffset: { width: -2, height: 4 },
-    shadowColor: "#171717",
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-  },
-  noCaptureText: {
-    textAlign: "center",
-    padding: 15,
-    fontWeight: "500",
-    backgroundColor: 'gray',
-    color:'white',
-    overflow: 'hidden',
-    borderRadius: 10,
-    marginTop: 15
-  },
-  captureButton: {
-    alignItems: "center",
-  },
-  captureButtonText: {
-    padding: 10,
-    textAlign: "center",
-    backgroundColor: "dodgerblue",
-    color: "white",
-    paddingBottom: 10,
-    overflow: "hidden",
-    borderRadius: 10,
-    width: 180,
-    marginBottom: 15,
-    fontWeight: "bold",
-  },
-  showRecordsButton: {
-    marginTop: 5,
-    padding: 10,
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginTop: 5,
-  },
-  recordsContainer: {
-    marginTop: 5,
-    padding: 10,
-    backgroundColor: "gainsboro",
-    borderRadius: 10,
-  },
-  boldText: {
-    fontWeight: "bold",
-    marginBottom: 8,
-    marginTop: 4,
-  },
-  captureInfo: {
-    marginLeft: 5,
-  },
-  captureSeparator: {
-    marginBottom: 2,
-  },
-  statsRow: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 3,
-  },
-  captureRow: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 3,
-  },
-});
